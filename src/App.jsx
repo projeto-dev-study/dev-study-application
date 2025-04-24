6cdc4c5aa8cc7265d2264b3e6402728024ad65e1
edsonplz <edson.silva@dcx.ufpb.br>
2025-04-24 17:27:51 -0300

    Tela de Apresentação de Atividade Concluida!

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import './App.css'
 import Apresentation from './pages/Apresentation/Apresentation'
 import Login from './pages/Login/Login'
 import EscolhasUnidades from './pages/EscolhasUnidades/EscolhasUnidades'
+import Register from './pages/Register/Register'
+import ResultCongratulation from './pages/ResultCongratulations/ResultCongratulation'
+import ApresentationATV from './pages/ApresentationATV/ApresentationATV'
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Login/> 
-      <Apresentation/>
-      */}
-      <EscolhasUnidades/>
+      {/* <Login/>  */}
+      {/* <Register/> */}
+      {/* <Apresentation/> */}
+      {/* <EscolhasUnidades/> */}
+      {/* <ResultCongratulation/> */}
+      <ApresentationATV/>
     </div>
   )
 }
